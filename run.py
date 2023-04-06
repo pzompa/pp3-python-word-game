8e7f0d8bcfbfcf5e2405db60913081c7200c2ecd
pzompa <>
2023-04-06 17:49:46 +0000

    define basic structure of the game

--- run.py
+++ run.py
@@ -1,3 +1,73 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Project 3: Python Project - Word Game 
+"""
+
+def random_word_function():
+    """
+    Generate a random Word
+    """
+    # Generate randomd Word
+    # create a list of random words
+    # choose a word form the list
+
+def welcome_screen():
+    """
+    Initial Game Screen
+    """
+    # Initial Game Screen
+    # draw initial screen
+    # anykey to continue
+
+
+def player_name_screen():
+    """
+    Get Player Name, to personalize the Game
+    """
+    # Ask for Player's Name
+    # request player's name
+
+def options_screen():
+    """
+    Options Menu
+    """
+    # Play or Exit
+
+def game_screen():
+    """
+    Main Game Code
+    """
+    # Main Game Code
+    # accept user input
+    # check input validity
+
+def congrats_screen():
+    """
+    Display Contratulation Screen
+    """
+    # draw Contratulation Screen
+
+def failed_screen():
+    """
+    Display Failed Screen
+    """
+    # Player loses
+    # draw failed screen
+
+def goodbye_screen():
+    """
+    Game end splash screen
+    """
+    # draw goodbye screen
+
+def main():
+    """
+    Start Game
+    """
+    # program main control flow
+    # call welcome screen
+    # call player name screen
+    # call option screen
+    # call game screen
+    # call congrats screen
+    # call failed screen
+    # call goodbye screen
